0f810f834c0e0aa1f238dd688c882a38dd633aaf
RyanD64 <3625@holbertonschool.com>
2022-08-17 08:07:28 +0000

    remodel

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install flask from pip3 using puppet
-exec {'sudo pip3 install flask -v 2.1.0':
-    command => 'sudo pip3 install flask -v 2.1.0',
-    path    => '/usr/bin',
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }

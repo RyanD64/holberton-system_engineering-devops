05e977cb2837abb06d7c3f25b78ef277165ac1ed
RyanD64 <3625@holbertonschool.com>
2022-09-20 08:48:45 +0000

    test

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     name = user.json().get("username")
     file = id + ".csv"
 
-    with open('file', 'w') as result:
+    with open(file, 'w') as result:
         write = csv.writer(result,
                            delimiter=",",
                            quotechar='"',
